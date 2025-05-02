8f77e387f93fbdfd94cc5e5dd46d1ee5167ac1d9
Burger-karl <charleskalu863@gmail.com>
2025-05-02 04:14:36 +0100

    admin review urls

--- reviews/urls.py
+++ reviews/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import ReviewCreateAPIView
+from .views import ReviewCreateAPIView, AdminReviewListAPIView
 
 urlpatterns = [
     path('reviews/', ReviewCreateAPIView.as_view(), name='review-create'),
+    path('admin/reviews/', AdminReviewListAPIView.as_view(), name='admin-review-list'),
 ]
