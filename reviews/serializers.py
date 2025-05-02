612289d549c313f1ec864cc69c5d24271651f55e
Burger-karl <charleskalu863@gmail.com>
2025-05-02 03:32:09 +0100

    reviews commit

---
+++ reviews/serializers.py
@@ -0,0 +1,63 @@
+# from rest_framework import serializers
+# from .models import Review
+# from user.models import User
+
+# class ReviewSerializer(serializers.ModelSerializer):
+#     user = serializers.PrimaryKeyRelatedField(
+#         queryset=User.objects.all(),
+#         default=serializers.CurrentUserDefault()
+#     )
+    
+#     class Meta:
+#         model = Review
+#         fields = ['id', 'user', 'rating', 'message', 'created_at']
+#         read_only_fields = ['id', 'user', 'created_at']
+#         extra_kwargs = {
+#             'rating': {
+#                 'min_value': 1,
+#                 'max_value': 5,
+#                 'error_messages': {
+#                     'min_value': 'Rating must be at least 1',
+#                     'max_value': 'Rating cannot be more than 5'
+#                 }
+#             },
+#             'message': {'required': False, 'allow_blank': True}
+#         }
+
+
+from rest_framework import serializers
+from .models import Review
+from user.models import User
+
+class ReviewSerializer(serializers.ModelSerializer):
+    user = serializers.PrimaryKeyRelatedField(
+        queryset=User.objects.all(),
+        default=serializers.CurrentUserDefault(),
+        required=False  # Make user not required for admin updates
+    )
+    user_email = serializers.SerializerMethodField(read_only=True)
+    
+    class Meta:
+        model = Review
+        fields = ['id', 'user', 'user_email', 'rating', 'message', 'created_at']
+        read_only_fields = ['id', 'created_at', 'user_email']
+        extra_kwargs = {
+            'rating': {
+                'min_value': 1,
+                'max_value': 5,
+                'error_messages': {
+                    'min_value': 'Rating must be at least 1',
+                    'max_value': 'Rating cannot be more than 5'
+                }
+            },
+            'message': {'required': False, 'allow_blank': True}
+        }
+    
+    def get_user_email(self, obj):
+        return obj.user.email if obj.user else None
+
+    def create(self, validated_data):
+        # Ensure user is set to current user when creating
+        if 'user' not in validated_data:
+            validated_data['user'] = self.context['request'].user
+        return super().create(validated_data)
