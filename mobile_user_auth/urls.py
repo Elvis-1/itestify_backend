01892c4cd22261bbe3ada2535a27fdd6b13d9634
michealdayo64 <omotoshomicheal93@gmail.com>
2025-05-19 08:52:34 +0100

    admin reset pass

--- mobile_user_auth/urls.py
+++ mobile_user_auth/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import (UserRegisterAPIView, LoginAPIView, SendPasswordResetOtpView, ForgotPasswordView, ResetPasswordView,
+from .views import (UserRegisterAPIView, LoginAPIView, SendPasswordResetOtpView,
                     SetNewPasswordView, GetRegisteredUsers, VerifyOtpView, DeleteUserAccount, ResendEmailVerificationOtpView, LogOutApiView)
 
 
@@ -13,7 +13,5 @@ urlpatterns = [
     path("get_users", GetRegisteredUsers.as_view()),
     path("delete", DeleteUserAccount.as_view()),
     path('logout', LogOutApiView.as_view()),
-    path('forgot-password/', ForgotPasswordView.as_view(), name='forgot-password'),
-    path('reset-password/<str:uidb64>/<str:token>/',
-         ResetPasswordView.as_view(), name='reset-user-pass'),
+
 ]
--- user/urls.py
+++ user/urls.py
@@ -1,6 +1,6 @@
 from rest_framework.routers import DefaultRouter
 from django.urls import path, include
-from .views import DashboardViewSet, LoginViewSet, UsersViewSet
+from .views import DashboardViewSet, LoginViewSet, UsersViewSet, ForgotPasswordView, ResetPasswordView
 
 
 router = DefaultRouter()
@@ -9,7 +9,11 @@ router.register(r'dashboard', DashboardViewSet, basename="dashboard")
 router.register(r'users', UsersViewSet, basename="users")
 
 
+
 urlpatterns = [
     path('', include(router.urls)),
+    path('forgot-password/', ForgotPasswordView.as_view(), name='forgot-password'),
+    path('reset-password/<str:uidb64>/<str:token>/',
+         ResetPasswordView.as_view(), name='reset-user-pass'),
 
 ]
