0fdfc50d8fe179baaae0c1591f88c730332491d7
Jerry <jegoisgs66@gmail.com>
2025-05-31 11:04:53 +0100

    +update db

--- itestify_backend/settings.py
+++ itestify_backend/settings.py
@@ -150,11 +150,11 @@ else:
     DATABASES = {
         'default': {
             "ENGINE": "django.db.backends.postgresql",           
-            "USER": os.getenv("USER"),
-            "PASSWORD":os.getenv("PASSWORD"),
-            "HOST": os.getenv("HOST"),
-            "PORT": os.getenv("PORT"),
-            "NAME": os.getenv("DBNAME"),
+            "USER": os.getenv("DB_USER"),
+            "PASSWORD":os.getenv("DB_PASSWORD"),
+            "HOST": os.getenv("DB_HOST"),
+            "PORT": os.getenv("DB_PORT"),
+            "NAME": os.getenv("DB_NAME"),
         }
     }
 
