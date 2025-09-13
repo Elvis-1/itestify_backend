af88a3c60e59aa9a3206b24ecdc30ef3c0d6aa80
michealdayo64 <omotoshomicheal93@gmail.com>
2025-09-13 03:12:26 +0100

    Notification

--- notifications/views.py
+++ notifications/views.py
@@ -1,9 +1,8 @@
 
-import json
-from django.conf import settings
+
 # import redis
-from notifications.consumers import REDIS_PREFIX
-from notifications.utils import notify_user_via_websocket
+from common.paginations import StandardResultsSetPagination
+
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 from notifications.models import Notification
@@ -11,6 +10,8 @@ from notifications.serializers import NotificationSerializer
 from user.models import User
 from common.responses import CustomResponse
 from common.error import ErrorCode
+from django.db.models import Q
+from django.utils.dateparse import parse_date
 # import asyncio
 # from .tasks import delayed_delete
 # from django.core.cache import cache
@@ -22,12 +23,13 @@ from common.error import ErrorCode
 class UnreadNotificationsView(APIView):
     permission_classes = [IsAuthenticated]
     serializer_class = NotificationSerializer
+    pagination_class = StandardResultsSetPagination
 
     # Post Selected Notifications as Read
     def post(self, request):
         user = request.user
         selected_notifications = request.data.get("selected_notifications")
-        print(selected_notifications)
+        delete_selected = request.query_params.get("delete_selected", None)
 
         try:
             user_id = User.objects.get(id=user.id)
@@ -37,15 +39,27 @@ class UnreadNotificationsView(APIView):
                 err_code=ErrorCode.NOT_FOUND,
                 status_code=404,
             )
+
         try:
             if user_id.role.name == "Admin" or user_id.role.name == "Super Admin":
+                if delete_selected == "True":
+                    Notification.objects.filter(
+                        id__in=selected_notifications).delete()
+                    return CustomResponse.success(
+                        message="Notification(s) deleted successfully for admin", status_code=200
+                    )
                 Notification.objects.filter(
                     id__in=selected_notifications, read=False).update(read=True)
-
                 return CustomResponse.success(
                     message="Notification marked as read successfully for admin", status_code=200
                 )
             elif user_id.role.name == "User":
+                if delete_selected == "True":
+                    Notification.objects.filter(
+                        id__in=selected_notifications).delete()
+                    return CustomResponse.success(
+                        message="Notification(s) deleted successfully for user", status_code=200
+                    )
                 Notification.objects.filter(
                     id__in=selected_notifications, read=False).update(read=True)
 
@@ -62,6 +76,11 @@ class UnreadNotificationsView(APIView):
     # Get All Notifications for User and Admin
     def get(self, request):
         user = request.user
+        from_date = request.query_params.get("from")
+        to_date = request.query_params.get("to")
+        search = request.query_params.get("search", "").strip()
+        read = request.query_params.get("read", "").strip()
+
         try:
             user_id = User.objects.get(id=user.id)
         except User.DoesNotExist:
@@ -70,35 +89,84 @@ class UnreadNotificationsView(APIView):
                 err_code=ErrorCode.NOT_FOUND,
                 status_code=404,
             )
-        notification = Notification.objects.all()
 
+        notification = Notification.objects.all()
+        paginator = self.pagination_class()
         if user_id.role.name == "User":
+            if search:
+                notification = notification.filter(
+                    Q(verb__icontains=search, target=user_id)
+                    | Q(message__icontains=search, target=user_id)
+                ).order_by("-timestamp")
+
+            if read == "True":
+                notification = notification.filter(read=True, target=user_id).order_by(
+                    "-timestamp")
+
+            if read == "False":
+                notification = notification.filter(read=False, target=user_id).order_by(
+                    "-timestamp")
+
+            if from_date:
+                parsed_from_date = parse_date(from_date)
+                if parsed_from_date:
+                    notification = notification.filter(
+                        timestamp__date__gte=parsed_from_date, target=user_id
+                    )
+
+            if to_date:
+                parsed_to_date = parse_date(to_date)
+                if parsed_to_date:
+                    # Set time to the end of the day for inclusivity
+                    notification = notification.filter(
+                        timestamp__date__lte=parsed_to_date, target=user_id)
 
             notification = notification.filter(target=user_id).order_by(
                 "-timestamp"
             )
-            serializer = self.serializer_class(notification, many=True)
-            return CustomResponse.success(
-                message="Notifications retrieved successfully for user",
-                data=serializer.data,
-                status_code=200,
-            )
+            notification_data = paginator.paginate_queryset(
+                notification, request)
+            serializer = self.serializer_class(notification_data, many=True)
+            return paginator.get_paginated_response(serializer.data)
 
         elif user_id.role.name == "Admin" or user_id.role.name == "Super Admin":
+            if search:
+                notification = notification.filter(
+                    Q(verb__icontains=search, role="Admin")
+                    | Q(message__icontains=search, role="Admin")
+                ).order_by("-timestamp")
+
+            if read == "True":
+                notification = notification.filter(read=True, role="Admin").order_by(
+                    "-timestamp")
+
+            if read == "False":
+                notification = notification.filter(read=False, role="Admin").order_by(
+                    "-timestamp")
+
+            if from_date:
+                parsed_from_date = parse_date(from_date)
+                if parsed_from_date:
+                    notification = notification.filter(
+                        timestamp__date__gte=parsed_from_date, role="Admin"
+                    )
+
+            if to_date:
+                parsed_to_date = parse_date(to_date)
+                if parsed_to_date:
+                    # Set time to the end of the day for inclusivity
+                    notification = notification.filter(
+                        timestamp__date__lte=parsed_to_date, role="Admin")
+
             notification = notification.filter(role="Admin").order_by(
                 "-timestamp"
             )
-            serializer = self.serializer_class(notification, many=True)
-            return CustomResponse.success(
-                message="Notifications retrieved successfully for admin",
-                data=serializer.data,
-                status_code=200,
-            )
-        return CustomResponse.error(
-            message="Notifications not found",
-            status_code=200,
-            err_code=ErrorCode.NOT_FOUND,
-        )
+
+        notification_data = paginator.paginate_queryset(
+            notification, request)
+        serializer = self.serializer_class(
+            notification_data, many=True)
+        return paginator.get_paginated_response(serializer.data)
 
     # Mark Single Notification as Read for User and Admin
     def put(self, request, id):
